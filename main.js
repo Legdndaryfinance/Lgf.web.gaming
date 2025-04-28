c0340a6a19f102745ed4b1cb9a23f3be85853fd9
Legendaryfinance <98645803+Legdndaryfinance@users.noreply.github.com>
2025-04-28 10:36:26 +0630

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// main.js
 document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.querySelector('.hamburger');
     const sidebar = document.querySelector('.sidebar');
